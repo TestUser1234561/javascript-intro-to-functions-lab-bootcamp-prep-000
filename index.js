531b7b02b7b464820c45b1bec75d00bd2978ecaa
Morgan Fudge <mfudge92@gmail.com>
2017-09-28 16:40:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 function shout(string) {
   return string.toUpperCase()
 }
-
-whisper = (str) => {
-  return str.toLowerCase();
-}
-
-logShout = (str) => {
-  console.log(str.toUpperCase());
-}
-
-logWhisper = (str) => {
-  console.log(str.toLowerCase());
-}
